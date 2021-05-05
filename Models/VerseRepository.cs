59ad9309f85b51619288c5ee521b38d36cbee618
Jamey McElveen <jameymcelveen@Jameys-MBP.lan>
2021-05-05 12:20:08 -0400

    First commit

---
+++ Models/VerseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace VerseOfTheDay.Models
+{
+    public interface IVerseRepository
+    {
+
+    }
+
+    public class VerseRepository
+    {
+        public VerseRepository()
+        {
+        }
+    }
+}
